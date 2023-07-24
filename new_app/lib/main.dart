536d952e845351671341393479c0f88fc68b1590
Ishwar Chaudhary <ishwarura@gmail.com>
2023-07-24 20:18:08 +0545

    Update main.dart
    Making Widget Tree

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(); //this is callled as a function which is provided by flutter
+  runApp(const MaterialApp(
+    home: Text('Ishwar Chaudhary'),
+  ));
 }
