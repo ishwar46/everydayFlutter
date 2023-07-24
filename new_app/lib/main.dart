f0f2b24b4e63c6f659ca7e2dac6f5ee1f37736e9
Ishwar Chaudhary <ishwarura@gmail.com>
2023-07-24 21:41:21 +0545

    Update main.dart
    adding gradient

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   //material app widget is the root widget of the app
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Ishwar Chaudhary'),
+        backgroundColor: Color.fromARGB(255, 73, 172, 162),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.teal, Color.fromARGB(255, 237, 174, 151)],
+            ),
+          ),
+          child: const Center(
+            child: Text('Ishwar Chaudhary'),
+          ),
         ),
       ),
     ),
