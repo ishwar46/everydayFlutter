6fc4ac65f7b516f6c446653d1d668d28dd110474
Ishwar Chaudhary <ishwarura@gmail.com>
2024-04-07 12:38:28 +0545

    Input Field Added

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,28 +11,36 @@ class Expenses extends StatefulWidget {
   }
 }
 
-final List<Expense> _registeredExpenses = [
-  Expense(
-    title: 'Flutter Course',
-    amount: 19.999,
-    date: DateTime.now(),
-    category: Category.work,
-  ),
-  Expense(
-    title: 'Movie',
-    amount: 12.69,
-    date: DateTime.now(),
-    category: Category.leisure,
-  ),
-  Expense(
-    title: 'Fruit Jucie',
-    amount: 12.69,
-    date: DateTime.now(),
-    category: Category.food,
-  ),
-];
-
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.999,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Movie',
+      amount: 12.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Fruit Jucie',
+      amount: 12.69,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return NewExpense();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +49,9 @@ class _ExpensesState extends State<Expenses> {
         title: const Text('Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              _openAddExpenseOverlay();
+            },
             icon: const Icon(Icons.add),
           ),
         ],
